e2d19847dc774778bb076c45dd3ba4b0bf54a5b2
hellonun <43077909+hellonun@users.noreply.github.com>
2022-05-07 15:47:26 -0400

    Update sketch.js

--- gameoflife/sketch.js
+++ gameoflife/sketch.js
@@ -1,23 +1,38 @@
-let w;
-let columns;
-let rows;
-let board;
-let pboard;
-let next;
+// KEYS
+// SPACE = REGENERATE
+// 1,2,3,4 = PRESET DENSITIES
+// UP, DOWN = FADING SPEED
+// LEFT, RIGHT = JUMP PRESENT DENSITIES
+// A = AUTO VS. MANUAL (AUTO MEANS WEIGHTED RANDOM)
+
+let w, h, columns, rows, board, pboard, next;
+let autoState = true;
 let fadeIn = 0;
 let fadeOut = 255;
-let runCount = 0;
-let countBlack = 0;
+let generateInverval = 300;
+let seedWeights = [0, 10, 20, 50, 20]; // DENSITY WEIGHT, IGNORE FIRST '0'
+let weights = [];
+let densities = [0.05, 0.1, 0.5, 0.75]; // DENSITY PRESET
+let di = 2; // CHANGE STARTING DENSITY HERE
+let density = densities[di];
+let num = 5;
+let whiteSquareCount;
 
 function setup() {
-  noStroke();
+  tWeight = 0;
+  for (i = 0; i < seedWeights.length; i++) {
+    tWeight = tWeight + seedWeights[i];
+    weights.push(tWeight);
+  }
 
+  noStroke();
   createCanvas(windowWidth, windowHeight);
   background(0);
-  w = windowWidth/5;
+  w = windowWidth / num;
+  h = windowHeight / num;
   // Calculate columns and rows
-  columns = floor(width / w);
-  rows = floor(height / w);
+  columns = num + 2;
+  rows = num + 2;
   // Wacky way to make a 2D array is JS
   board = new Array(columns);
   for (let i = 0; i < columns; i++) {
@@ -34,16 +49,17 @@ function setup() {
 function draw() {
   // background(255);
 
-  if (frameCount % 600 == 0) {
+  translate(-w, -h);
+
+  if (frameCount % generateInverval == 0) {
     fadeIn = 0;
     fadeOut = 255;
     pboard = board;
     generate();
   }
 
-  // fade out previous board
-
   // draw new board
+  changeCount = 0;
   if (pboard) {
     for (let i = 0; i < columns; i++) {
       for (let j = 0; j < rows; j++) {
@@ -53,18 +69,24 @@ function draw() {
         } else if (pboard[i][j] != board[i][j] && pboard[i][j] == 1) {
           // if it used to be white
           fill(fadeOut);
+          changeCount++;
         } else if (pboard[i][j] != board[i][j] && pboard[i][j] == 0) {
           // if it used to be black
           fill(fadeIn);
+          changeCount++;
         } else {
           // if both are black
           fill(0);
         }
         noStroke();
-        rect(i * w, j * w, w, w);
+        rect(i * w, j * h, w, h);
       }
     }
+    if (autoState && changeCount == 0) {
+      autoDrive();
+    }
   } else {
+    // first board
     for (let i = 0; i < columns; i++) {
       for (let j = 0; j < rows; j++) {
         if (board[i][j] == 1) {
@@ -73,16 +95,27 @@ function draw() {
           fill(0);
         }
         noStroke();
-        rect(i * w, j * w, w, w);
+        rect(i * w, j * h, w, h);
       }
     }
   }
-  fadeIn += 1;
-  fadeOut -= 1;
+  fadeIn += 1 * (600 / generateInverval);
+  fadeOut -= 1 * (600 / generateInverval);
+
+  fill(255, 0, 0);
+  textSize(40);
+  text("auto:" + autoState, width / 2 + w, height - 130);
+  text("speed:" + generateInverval, width / 2 + w, height - 90);
+  text("density:" + density, width / 2 + w, height - 50);
 }
 
-// reset board when mouse is pressed
-function mousePressed() {
+function autoDrive() {
+  r = random(100);
+  for (i = 0; i < weights.length; i++) {
+    if (r > weights[i] && r < weights[i + 1]) {
+      density = densities[i];
+    }
+  }
   init();
 }
 
@@ -94,7 +127,7 @@ function init() {
       if (i == 0 || j == 0 || i == columns - 1 || j == rows - 1)
         board[i][j] = 0;
       // Filling the rest randomly
-      else board[i][j] = floor(random(2));
+      else board[i][j] = random(1) <= density ? 1 : 0;
       next[i][j] = 0;
     }
   }
@@ -102,6 +135,7 @@ function init() {
 
 // The process of creating the new generation
 function generate() {
+  console.log("generate");
   // Loop through every spot in our 2D array and check spots neighbors
   for (let x = 1; x < columns - 1; x++) {
     for (let y = 1; y < rows - 1; y++) {
@@ -131,4 +165,42 @@ function generate() {
   let temp = board;
   board = next;
   next = temp;
+}
+
+function keyPressed() {
+  // if user presses 1, decrease generate interval by 50 until it reaches 300, if user presses 2, increase generate interval by 50 until it reaches 900
+
+  if (key == "1") {
+    density = densities[0];
+  } else if (key == "2") {
+    density = densities[1];
+  } else if (key == "3") {
+    density = densities[2];
+  } else if (key == "4") {
+    density = densities[3];
+  } else if (keyCode == UP_ARROW) {
+    generateInverval += 50;
+  } else if (keyCode == DOWN_ARROW) {
+    generateInverval -= 50;
+  } else if (keyCode == LEFT_ARROW) {
+    if (di > 0) {
+      di--;
+    }
+    density = densities[di];
+  } else if (keyCode == RIGHT_ARROW) {
+    if (di < densities.length - 1) {
+      di++;
+    }
+    density = densities[di];
+  } else if (key == " ") {
+    init();
+  } else if (key == "a") {
+    autoState = !autoState;
+  }
+
+  generateInverval = constrain(generateInverval, 60, 900);
+  // density = constrain(density, 0, 1);
+
+  console.log("current generation interval: " + generateInverval);
+  console.log("current speed: " + density);
 }
